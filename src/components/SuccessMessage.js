bde397fc49986502af2ea85104f812b4e762f96e
Vimalan U <vimalanu.21it@kongu.edu>
2024-03-18 13:38:04 +0530

    ecart
    ecart

---
+++ src/components/SuccessMessage.js
@@ -0,0 +1,9 @@
+const SuccessMessage = () => {
+  return (
+    <section className="success-alert">
+      <p>successfully added to cart!</p>
+    </section>
+  );
+}
+
+export default SuccessMessage;
